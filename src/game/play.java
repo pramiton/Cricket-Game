d50833efd24ed98399546bff7ef314b01be3f05c
PranTanTheMan <Seriousbro23@gmail.com>
2022-01-11 10:54:59 -0600

    Wrong comit lol

--- src/game/play.java
+++ src/game/play.java
@@ -1,9 +1,5 @@
 package game;
 
-public class play {
-    public interface PhoneBook {
-        public void ReadInfo();
-        public String FindNumber(String name);
-        public String FindName(String phonenumber);
-    }
+public class play{
+    
 }
