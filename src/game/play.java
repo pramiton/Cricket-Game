c76fbb11b25316f54638b89975a3cc11a6e077a3
PranTanTheMan <Seriousbro23@gmail.com>
2022-01-11 10:53:45 -0600

    second

--- src/game/play.java
+++ src/game/play.java
@@ -1,5 +1,9 @@
 package game;
 
 public class play {
-
+    public interface PhoneBook {
+        public void ReadInfo();
+        public String FindNumber(String name);
+        public String FindName(String phonenumber);
+    }
 }
