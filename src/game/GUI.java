174109bb51acb25ce858612e8211ce294be2b6a8
PranTanTheMan <Seriousbro23@gmail.com>
2022-01-13 11:04:19 -0600

    setup

--- src/game/GUI.java
+++ src/game/GUI.java
@@ -4,6 +4,16 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class GUI {
+public class GUI implements ActionListener{
 
+    public GUI(){
+        
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO Auto-generated method stub
+
+    }
+    
 }
